30dc916c3aaa3a67b64a4286a0bcb70987c52d67
Israel Nunes <israelnetonunes@gmail.com>
2024-05-29 11:04:01 -0300

    final project

---
+++ swiper.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 10,
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets'
+    },
+});
+
+
+const swiper__diff = new Swiper('.swiper__diff', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets'
+    }, 
+});
